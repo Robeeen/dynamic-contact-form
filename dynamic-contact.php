5ba6f2b4699b19677c45d8c8fea113525684eee6
Robeeen <robeeen@gmail.com>
2024-10-05 09:42:37 +0600

    first

---
+++ dynamic-contact.php
@@ -0,0 +1,25 @@
+<?php
+
+
+ /*
+ * Plugin Name: Dasborad Plugin
+ * Description: A plugin to create custom dashboards for users with registration, form input, and table display.
+ * Version: 1.0.0
+ * Requires at least: 5.0
+ * Requires PHP: 7.0
+ * Author: Shams Khan
+ * Author URI: https://shamskhan.com
+ * License: GPL-2.0+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:
+ * Text Domain: custom-table
+ * Domain Path: /languages/asset/
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+//plugin Versions
+defined( 'PLUGIN_NAME_VERSION', '1.0.0' );
